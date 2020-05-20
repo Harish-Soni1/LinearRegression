58bc6d5b3b7b666bfa52c85ce310f4717b006c87
Harish <57593127+Harish-Soni1@users.noreply.github.com>
2020-05-20 16:32:33 +0530

    Update and rename EmployeeLinearModel to EmployeeLinearModel.py

--- Using Python/EmployeeLinearModel.py
+++ Using Python/EmployeeLinearModel.py
@@ -1,4 +1,9 @@
-Employee Dataset
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+#Employee Dataset
 
 df2=pd.read_csv("E:\\itsstudytym\\assignments\\SLR\\emp_data SLR.csv")
 
